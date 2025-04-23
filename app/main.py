0d31adeb4bac35d89e20120a7e9e3379638974db
pablomartinvelasco <pablomartinvelasco@outlook.com>
2025-04-23 22:30:46 +0200

    feat: permite guardar training_date manualmente, y añade listado de entrenamientos con GET /trainings

--- app/main.py
+++ app/main.py
@@ -1,22 +1,27 @@
 # app/main.py
 
 from fastapi import FastAPI
-from sqlmodel import Session
+from sqlmodel import Session, select
+from app.models import Training, TrainingCreate
 from app.database import create_db_and_tables, engine
-from app.models import Training
 
 app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Running Progress Tracker API funcionando correctamente"}
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
+@app.get("/")
+def root():
+    return {"message": "Running Progress Tracker API funcionando correctamente"}
+
 @app.post("/training")
-def create_training(training: Training):
+def create_training(training_in: TrainingCreate):
+    training_data = training_in.dict()
+    if training_data["training_date"] is None:
+        from datetime import date
+        training_data["training_date"] = date.today()
+    training = Training(**training_data)
     with Session(engine) as session:
         session.add(training)
         session.commit()
@@ -25,3 +30,10 @@ def create_training(training: Training):
         "message": "Entrenamiento guardado en la base de datos",
         "data": training.model_dump()
     }
+
+@app.get("/trainings")
+def read_trainings():
+    with Session(engine) as session:
+        statement = select(Training)
+        trainings = session.exec(statement).all()
+        return trainings
--- app/models.py
+++ app/models.py
@@ -4,9 +4,15 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import date
 
-class Training(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class TrainingBase(SQLModel):
     distance_km: float
-    pace_seconds_per_km: float
+    pace_seconds_per_km: int
     notes: str
-    date: date = Field(default_factory=date.today)
+    training_date: Optional[date] = None
+
+class Training(TrainingBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    training_date: Optional[date] = Field(default_factory=date.today)
+
+class TrainingCreate(TrainingBase):
+    pass
